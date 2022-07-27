2c27a639200396184f8c6af7454e5099c8b9ac48
szucsmate <szucsmate096@gmail.com>
2022-07-27 17:46:01 +0200

    setup starting files with base style and components

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,20 @@ import styles from './Header.module.css'
 
 const Header = () => {
     return (
-        <div className='header'>
-            <h3 className={styles.main}>
-                Helloo
-            </h3>
-            <h4 className={styles.secondary}>
-                World!
-            </h4>
+        <div className={styles.header}>
+            <h1 className={styles.main}>
+                Simple, traffic-based pricing
+            </h1>
+            <div className={styles.secondary}>
+                <h4>
+                    Sign-up for our 30-day trial.
+                </h4>
+                <h4>
+                    No credit card required.
+                </h4>
+            </div>
+
+
         </div>
 
     )
