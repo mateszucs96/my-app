8fa74b03a7f50845debd00bb51c58a2524debe0b
szucsmate <szucsmate096@gmail.com>
2022-07-28 20:53:22 +0200

    add toggle button functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,51 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Slider from './Slider.js';
-
-import styles from './card.module.scss'
+import styles from './card.module.scss';
+import iconCheck from '../images/icon-slider.svg'
 
 const Card = () => {
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState('100K');
+    const [price, setPrice] = useState((16).toFixed(2));
+    const [isMonthly, setIsMonthly] = useState(true);
+
+
+    useEffect(() => {
+        if (isMonthly === true) {
+            console.log('a')
+        }
+    }, [isMonthly])
+
+    const toggleButton = (e) => {
+        const clicked = e.target.closest(`.${styles.toggleContainer}`);
+        if (clicked.style.justifyContent === 'flex-start' || clicked.style.justifyContent === '') {
+            clicked.style.justifyContent = 'flex-end';
+            setIsMonthly(false);
+        } else {
+            clicked.style.justifyContent = 'flex-start';
+            setIsMonthly(true);
+        }
+
+    }
+
+
 
 
     return (
         <div className={styles.container}>
             <div className={styles.pageview}>
-                <h4>100K PAGEVIEWS</h4>
+                <h4>{value} PAGEVIEWS</h4>
             </div>
-            <Slider />
+            <Slider changeValue={setValue} changePrice={setPrice} />
+            <div className={styles.billingTag}>
+                <h4 className={styles.price}>${price.toString()} <span className={styles.monthly}>/ month</span></h4>
+            </div>
+            <div className={styles.billing}>
+                <h5 className={styles.text}>Monthly Billing</h5>
+                <div className={styles.toggleContainer} onClick={toggleButton}>
+                    <div className={styles.button}></div>
+                </div>
+                <h5 className={styles.text}>Yearly Billing</h5>
+            </div>
         </div>
     )
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,17 +2,49 @@ import React from 'react'
 
 import styles from './slider.module.scss';
 
-const Slider = () => {
-    function con(e) {
+const Slider = (props) => {
+    const sliding = (e) => {
         const value = +e.target.value;
         const width = +e.target.clientWidth;
         e.target.nextSibling.style.marginLeft = (5 * value) * (width / 10) + 'px';
     }
 
+    const updateText = (e) => {
+        const texts = ['10K', '50K', '100K', '500K', '1M'];
+        props.changeValue(texts[+e.target.value + 2])
+    }
+
+    const updatePrice = (e) => {
+        const value = e.target.value;
+        console.log(value)
+        switch (value) {
+            case '-2':
+                props.changePrice((8).toFixed(2));
+                break;
+            case '-1':
+                props.changePrice((12).toFixed(2));
+                break;
+            case '1':
+                props.changePrice((24).toFixed(2));
+                break;
+            case '2':
+                props.changePrice((36).toFixed(2));
+                break;
+            default:
+                props.changePrice((16).toFixed(2));
+
+        }
+    }
+
 
     return (
         <div className={styles.container}>
-            <input type="range" min="-2" max="2" step="1" id={styles.slider} onInput={con} onChange={e => console.log(e.target.value)} />
+            <input type="range" min="-2" max="2" step="1" id={styles.slider}
+                onInput={sliding}
+                onChange={(e) => {
+                    updateText(e);
+                    updatePrice(e);
+                }} />
             <div className={styles.button}></div>
         </div>
     )
